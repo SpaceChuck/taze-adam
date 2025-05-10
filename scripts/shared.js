ce1388755fea7d3a7a30afb1387c7f089b90f0ac
SpaceChuck <60058320+SpaceChuck@users.noreply.github.com>
2025-05-10 17:53:45 +0300

    Fixed redirects, but now for the JS file

--- scripts/shared.js
+++ scripts/shared.js
@@ -2,14 +2,14 @@ class Header extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <div class="navbar">
-    <a target="_self" href="/index.html"><img src="/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
-    <a target="_self" href="/games">Games</a>
-    <a target="_self" href="/news">News</a>
-    <a target="_self" href="/other">Other</a>
+    <a target="_self" href="/taze-adam/index.html"><img src="/taze-adam/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
+    <a target="_self" href="/taze-adam/games">Games</a>
+    <a target="_self" href="/taze-adam/news">News</a>
+    <a target="_self" href="/taze-adam/other">Other</a>
     <div class="socials label">Follow SpaceChuck on: <br>
         <div class="list">
-            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
-            <a href="https://spacechucksblog.blogspot.com"><img src="/assets/Blogger.png" alt="Blogger"></a>
+            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/taze-adam/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
+            <a href="https://spacechucksblog.blogspot.com"><img src="/taze-adam/assets/Blogger.png" alt="Blogger"></a>
             <div class="empty"></div>
         </div>
     </div>
@@ -21,7 +21,7 @@ class Footer extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `    
         <div class="footer">
-            <a href="https://spacechuck.github.io/"><img src="/assets/SpaceChuckLogo.png"></img></a><br>
+            <a href="https://spacechuck.github.io/"><img src="/taze-adam/assets/SpaceChuckLogo.png"></img></a><br>
             <!-- <span style="color:#fa5c5c">English</span>
             <a href="tr">T&#252;rk&#231;e</a> -->
         </div>    
@@ -32,14 +32,14 @@ class HeaderGames extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <div class="navbar">
-    <a target="_self" href="/index.html"><img src="/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
-    <a target="_self" href="/games" class="active">Games</a>
-    <a target="_self" href="/news">News</a>
-    <a target="_self" href="/other">Other</a>
+    <a target="_self" href="/taze-adam/index.html"><img src="/taze-adam/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
+    <a target="_self" href="/taze-adam/games" class="active">Games</a>
+    <a target="_self" href="/taze-adam/news">News</a>
+    <a target="_self" href="/taze-adam/other">Other</a>
     <div class="socials label">Follow SpaceChuck on: <br>
         <div class="list">
-            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
-            <a href="https://spacechucksblog.blogspot.com"><img src="/assets/Blogger.png" alt="Blogger"></a>
+            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/taze-adam/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
+            <a href="https://spacechucksblog.blogspot.com"><img src="/taze-adam/assets/Blogger.png" alt="Blogger"></a>
             <div class="empty"></div>
         </div>
     </div>
@@ -51,14 +51,14 @@ class HeaderNews extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <div class="navbar">
-    <a target="_self" href="/index.html"><img src="/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
-    <a target="_self" href="/games">Games</a>
-    <a target="_self" href="/news" class="active">News</a>
-    <a target="_self" href="/other">Other</a>
+    <a target="_self" href="/taze-adam/index.html"><img src="/taze-adam/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
+    <a target="_self" href="/taze-adam/games">Games</a>
+    <a target="_self" href="/taze-adam/news" class="active">News</a>
+    <a target="_self" href="/taze-adam/other">Other</a>
     <div class="socials label">Follow SpaceChuck on: <br>
         <div class="list">
-            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
-            <a href="https://spacechucksblog.blogspot.com"><img src="/assets/Blogger.png" alt="Blogger"></a>
+            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/taze-adam/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
+            <a href="https://spacechucksblog.blogspot.com"><img src="/taze-adam/assets/Blogger.png" alt="Blogger"></a>
             <div class="empty"></div>
         </div>
     </div>
@@ -70,14 +70,14 @@ class HeaderOther extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <div class="navbar">
-    <a target="_self" href="/index.html"><img src="/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
-    <a target="_self" href="/games">Games</a>
-    <a target="_self" href="/news">News</a>
-    <a target="_self" href="/other" class="active">Other</a>
+    <a target="_self" href="/taze-adam/index.html"><img src="/taze-adam/assets/TazeAdamLogo.png" alt="Taze Adam"></a>
+    <a target="_self" href="/taze-adam/games">Games</a>
+    <a target="_self" href="/taze-adam/news">News</a>
+    <a target="_self" href="/taze-adam/other" class="active">Other</a>
     <div class="socials label">Follow SpaceChuck on: <br>
         <div class="list">
-            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
-            <a href="https://spacechucksblog.blogspot.com"><img src="/assets/Blogger.png" alt="Blogger"></a>
+            <a href="https://www.youtube.com/@SpaceChuck1"><img src="/taze-adam/assets/youtube_social_icon_red.png" alt="YouTube channel (SpaceChuck)"></a>
+            <a href="https://spacechucksblog.blogspot.com"><img src="/taze-adam/assets/Blogger.png" alt="Blogger"></a>
             <div class="empty"></div>
         </div>
     </div>
